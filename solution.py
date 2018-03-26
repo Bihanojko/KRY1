edd04df88b5ed99141f6e154510190c01c72a0b3
Nikola Valesova <bihanojko@dhcpf033.fit.vutbr.cz>
2018-03-26 17:29:54 +0200

    Reworked for new input files

--- solution.py
+++ solution.py
@@ -20,23 +20,51 @@ keystream = keystream[:32]
 
 
 # key scheduling algorithm
-SUB = [0, 1, 1, 0, 0, 1, 0, 1]
+SUB = [0, 1, 1, 0, 1, 0, 1, 0]
 N_B = 32
 N = 8 * N_B
 
-# Next keystream
-def step(x):
-    x = (x & 1) << N+1 | x << 1 | x >> N-1
-    y = 0
-    for i in range(N):
-        y |= SUB[(x >> i) & 7] << i
-    return y
+
+def getNext(currBit, i1, i2):
+    # print(str(currBit) + " , " + str(i1) + " , " + str(i2))
+    if currBit == 0:
+        if i1 == i2 == 0:
+            return 0
+        else:
+            return 1
+    else:
+        if i1 == i2 == 0:
+            return 1
+        else:
+            return 0
+
+
+def arrayToInt(x):
+    intFromArray = 0
+    for idx in range(len(x)):
+        if idx != 0 and idx != len(x) - 1:
+            intFromArray |= x[idx] << (N - idx)
+    return intFromArray
+
+
+def reversedStep(x):
+    for seq in [[0, 0], [0, 1], [1, 0], [1, 1]]:
+        keyArray = seq
+        for i in range(N):
+            currBit = (x >> (N - i - 1)) & 1
+            keyArray.append(getNext(currBit, keyArray[i + 1], keyArray[i]))
+        # print(arrayToInt(keyArray))
+        if (keyArray[0] == keyArray[N] and keyArray[1] == keyArray[N + 1]):
+            return arrayToInt(keyArray)
+
 
 # Keystream init
 keystr = int.from_bytes(keystream, 'little')
-for i in range(N//2):
-    keystr = step(keystr)
+
+for _ in range(N // 2):
+    keystr = reversedStep(keystr)
 
 # print secret key
-print((keystr.to_bytes(N_B, 'little')).decode("utf-8"))
-sys.exit(0)
+keystr = keystr.to_bytes(N_B, 'little')
+keystr = keystr[:29]
+print(keystr.decode("ascii"))
